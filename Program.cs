b2f1cc3ca5dce5bff7202dc159e20c45eddc52ad
Douglas S <94dousch@gafe.molndal.se>
2021-09-27 11:49:27 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TestCI
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
